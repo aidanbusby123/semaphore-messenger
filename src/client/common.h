c2ecd1a167d9074d28367e226539d1fb9a8bdb5e
Aidan Busby <aidanbusby123@gmail.com>
2023-07-07 08:46:28 -0700

    protocol work

--- src/client/common.h
+++ src/client/common.h
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<openssl/sha.h>
+
 #ifndef COMMON_H
 #define COMMON_H
 
@@ -7,34 +7,48 @@
 #define CA 0x02
 #define CON 0x03
 
+#define BUFLEN 1024
 #define KEY_SZ 4096
 
 #define PUBLIC 0
 #define PRIVATE 1
 
-#define PORT 9050
+#define PORT 8080
+#define TOR_PORT 9050
 
-extern const char MAGIC[3];
+// connection modes
+#define DEFAULT 0
+#define TOR 1
+//
+
+#define SHA256_SZ 32
+extern const char MAGIC[2];
 
 typedef struct msg{
     unsigned char type;
-    unsigned char *recv_pub_key; // receiver (user) public key
-    unsigned char *send_pub_key; // sender public key
-    unsigned char timestamp[21]; // timestamp of message
-    unsigned char sz[5];
-    unsigned char checksum[SHA256_DIGEST_LENGTH*2+1];
+    unsigned char *recv_addr; // receiver address (hash of public key)
+    unsigned char *send_addr; // sender address (hash of public key)
+    unsigned int timestamp; // timestamp of message
+    unsigned int sz;
+    unsigned char checksum[SHA256_SZ];
     unsigned char *cipher; // encrypted message
     unsigned char *content; // decrypted message content
 }msg;
 
+struct status{
+    int connected;
+};
+
 typedef struct ctx{
     int server_fd; // server socket fd
     int ui_sock; // UI socket
+    struct status stat;
+    unsigned char *addr;
     unsigned char *pub_key;
     unsigned char *priv_key;
 }ctx;
 
-int server_connect(char *addr_s, int port);
+int server_connect(char *addr_s, int port, int mode);
 
 int public_encrypt(unsigned char * data,int data_len,unsigned char * key, unsigned char *encrypted);
 int private_decrypt(unsigned char * enc_data,int data_len,unsigned char * key, unsigned char *decrypted);
@@ -45,11 +59,8 @@ int rsa_sz(unsigned char* key, int type);
 
 unsigned char* sha256(unsigned char *d, size_t n, unsigned char *md);
 
-unsigned char* char_to_hex(unsigned char *s); // convert raw char array to hex representatio
-
 int send_msg(msg message, int server_fd); // send message
-
-void *recv_msg_thread(void *arg); // receiving thread
+void *recv_msg(void *arg); // handle the reception of messages
 
 FILE* open_file(char *file);
 int fsize(FILE* fp);
